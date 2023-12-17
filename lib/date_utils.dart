c98c4826cb40bffe1a42e786fbb414ef4b7e06f3
muhamedsultan21 <muhamedsultan211@gmail.comm>
2023-12-17 12:24:58 +0200

    create calender format

--- lib/date_utils.dart
+++ lib/date_utils.dart
@@ -1,8 +1,12 @@
 import 'package:intl/intl.dart';
 
-class MyDateUtils{
-  static String formatTaskDate(DateTime dateTime){
+class MyDateUtils {
+  static String formatTaskDate(DateTime dateTime) {
     var formater = DateFormat("yyyy/MM/dd");
     return formater.format(dateTime);
+  }
+
+  static DateTime dayOnly(DateTime input) {
+    return DateTime(input.year, input.day, input.month);
   }
 }
